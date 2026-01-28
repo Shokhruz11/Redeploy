68f261f0ae411d6c213c45a7fc8b459f6be65d68
Shokhruz11 <tarix1232004@gmail.com>
2026-01-28 21:17:24 +0500

    Refactor bot.py to simplify message handling

--- bot.py
+++ bot.py
@@ -1,8 +1,4 @@
-pyTelegramBotAPI==4.14.0
-# UNIVERSAL TELEGRAM BOT (Slayd / Referat / AI)
-
 import telebot
-from telebot import types
 
 BOT_TOKEN = "8086850400:AAHUpWbBtn9Bl_PMQgYOlf5OlmC-NBB2z30"
 
@@ -10,11 +6,10 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Assalomu alaykum! Bot muvaffaqiyatli ishga tushdi ✔")
+    bot.reply_to(message, "Assalomu alaykum! Bot ishga tushdi!")
 
 @bot.message_handler(func=lambda message: True)
 def echo(message):
-    bot.reply_to(message, "Siz yozdingiz: " + message.text)
+    bot.reply_to(message, message.text)
 
-print("Bot ishga tushdi...")
 bot.polling(none_stop=True)
